c508fd5050dc95af5d1ee299418d75966a06760a
Bailey McDonald <bails@Baileys-MacBook-Pro.local>
2022-11-17 09:45:11 -0600

    connected to mongo atlas

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 //-----------------------------//
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 
 let PORT = 3000;
 if(process.env.PORT){
@@ -17,3 +19,9 @@ app.listen(PORT, ()=>{
 	console.log('listening');
 })
 
+
+
+
+mongoose.connect('mongodb+srv://musiclover:<password>@mymusic.wef1hdo.mongodb.net/?retryWrites=true&w=majority', ()=>{
+	console.log('connected to mongo');
+})
