4daf25f4dba0f326e4e5cfb44554b690fa3220ef
Bailey McDonald <bails@baileys-mbp.myfiosgateway.com>
2022-11-28 10:29:37 -0500

    submit comment button styled w/ bootstrap, edit post page has background color, reviewed by: on show page links to author page

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,13 +1,9 @@
-const { setInternalBufferSize } = require('bson');
 const express = require('express');
 const router = express.Router();
-const Author = require('../models/authors.js');
-const Post = require('../models/postschema.js');
-const user = require('../models/user.js');
 const UserModel = require('../models/user.js')
 
 
-//prevent pplp who ar not logged in from getting to restricted pages
+//prevent ppl who are not logged in from getting to restricted pages
 const isAuth = (req, res, next) => {
     if (req.session.isAuth) {
         next()
@@ -18,67 +14,17 @@ const isAuth = (req, res, next) => {
 
 //-----------ROUTES------------//
 
-//probably get rid of b/c don't need to make new authors
-router.get('/users/:userId/new', isAuth, (req, res)=>{
-	UserModel.findById(req.params.userId, (err, foundUser)=> {
-		res.render('authors/new.ejs',
-			{
-				user: foundUser
-			}
-		);
-
-	})
-});
-
-
-// //profile page
-// router.get('/profile/users/:userId', isAuth, (req, res)=>{
-// 	UserModel.findById(req.params.userId, (err, foundUser)=> {
-// 		console.log(foundUser);
-//         console.log(foundUser);
-// 		res.render('users/profile.ejs',
-// 		{
-// 			user:foundUser
-// 		}
-// 		)
-// 	})
-	
-// })
-
-
-
-router.post('/', (req, res)=>{
-	req.body.articles = [{album : req.body.name}]
-	console.log(req.body);
-	Author.create(req.body, (err, createdAuthor)=>{
-		
-		// Author.findOneAndUpdate({'name':req.body.name}, {'articles':[]}, (err, updatedAuthor)=>{
-		// 	console.log(updatedAuthor);
-			// Post.findOneAndDelete({'album':req.body.name}, (err, deletedPost)=>{
-				if(err) console.log(err);
-
-				res.redirect('/authors');
-			// })
-		// }) 
-
-		// createdAuthor.articles.pop() 
-		// console.log(createdAuthor.articles);
-		// console.log(createdAuthor);
-
-	});
-	
-});
 
 router.get('/user/:userId', isAuth, (req, res)=>{
 	UserModel.findById(req.params.userId,(err, foundUser)=> {
 		UserModel.find({}, (err, foundUsers)=> {
-			Author.find({}, (err, foundAuthors)=>{
+			// Author.find({}, (err, foundAuthors)=>{
 				res.render('authors/index.ejs', {
-					authors: foundAuthors,
+					// authors: foundAuthors,
 					user:foundUser,
 					users: foundUsers
 				});
-			})
+			// })
 		})
 		
 	})
@@ -97,43 +43,83 @@ router.get('/user/:userId/author/:authorId', isAuth, (req, res)=>{
 	
 });
 
-router.put('/:id', (req, res)=>{
-	Author.findByIdAndUpdate(req.params.id, req.body, ()=>{
-		res.redirect('/authors');
-	});
-});
-
-
-router.get('/:id/edit', isAuth, (req, res)=>{
-	Author.findById(req.params.id, (err, foundAuthor)=>{
-		res.render('authors/edit.ejs', {
-			author: foundAuthor
-		});
-	});
-});
-
-
-
-
-router.delete('/:id', (req, res)=>{
-	Author.findByIdAndRemove(req.params.id, (err, foundAuthor)=>{
-		const articleIds = [];
-		for (let i = 0; i < foundAuthor.articles.length; i++) {
-			articleIds.push(foundAuthor.articles[i]._id);
-		}
-		Post.remove(
-			{
-				_id : {
-					$in: articleIds
-				}
-			},
-			(err, data)=>{
-				res.redirect('/authors');
-			}
-		);
-	});
-});
-
 
 
 module.exports = router;
+
+//-----------GRAVEYARD------------//
+
+// const Author = require('../models/authors.js');
+// const Post = require('../models/postschema.js');
+// const user = require('../models/user.js');
+
+//--------------CREATE-NEW-AUTHOR------------//
+// router.get('/users/:userId/new', isAuth, (req, res)=>{
+// 	UserModel.findById(req.params.userId, (err, foundUser)=> {
+// 		res.render('authors/new.ejs',
+// 			{
+// 				user: foundUser
+// 			}
+// 		);
+
+// 	})
+// });
+
+//-------------POST-NEW-AUTHOR------------//
+// router.post('/', (req, res)=>{
+// 	req.body.articles = [{album : req.body.name}]
+// 	console.log(req.body);
+// 	Author.create(req.body, (err, createdAuthor)=>{
+		
+// 		// Author.findOneAndUpdate({'name':req.body.name}, {'articles':[]}, (err, updatedAuthor)=>{
+// 		// 	console.log(updatedAuthor);
+// 			// Post.findOneAndDelete({'album':req.body.name}, (err, deletedPost)=>{
+// 				if(err) console.log(err);
+
+// 				res.redirect('/authors');
+// 			// })
+// 		// }) 
+
+// 		// createdAuthor.articles.pop() 
+// 		// console.log(createdAuthor.articles);
+// 		// console.log(createdAuthor);
+
+// 	});
+	
+// });
+
+//--------------------EDIT AUTHOR --------------------//
+// router.get('/:id/edit', isAuth, (req, res)=>{
+// 	Author.findById(req.params.id, (err, foundAuthor)=>{
+// 		res.render('authors/edit.ejs', {
+// 			author: foundAuthor
+// 		});
+// 	});
+// });
+
+//------------EDIT-AUTHOR-PUT------------//
+// router.put('/:id', (req, res)=>{
+// 	Author.findByIdAndUpdate(req.params.id, req.body, ()=>{
+// 		res.redirect('/authors');
+// 	});
+// });
+
+//------------DELETE-AUTHOr------------//
+// router.delete('/:id', (req, res)=>{
+// 	Author.findByIdAndRemove(req.params.id, (err, foundAuthor)=>{
+// 		const articleIds = [];
+// 		for (let i = 0; i < foundAuthor.articles.length; i++) {
+// 			articleIds.push(foundAuthor.articles[i]._id);
+// 		}
+// 		Post.remove(
+// 			{
+// 				_id : {
+// 					$in: articleIds
+// 				}
+// 			},
+// 			(err, data)=>{
+// 				res.redirect('/authors');
+// 			}
+// 		);
+// 	});
+// });
