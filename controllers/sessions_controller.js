a2ee0eb13c05d1e7b2cfaeb9d89953264c4e34ac
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-11-28 16:54:14 -0500

    warning messages

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -4,11 +4,15 @@
 const express = require('express');
 const sessions = express.Router();
 const bcrypt = require('bcrypt');
+// const flash = require('connect-flash');
 
+
+// app.use(flash())
 
 const Post = require('../models/postschema.js');
 const UserModel = require('../models/user.js')
 const Author = require('../models/authors.js');
+const { application } = require('express');
 
 
 
@@ -23,37 +27,58 @@ const isAuth = (req, res, next) => {
     }
 }
 
-
+//-------------------------------//
 //-----------ROUTES------------//
+//-------------------------------//
 
 //---------ENTER SITE-----------//
 sessions.get('/', (req, res)=> {
+    const userNoExist = req.session.userNoExist
+    const wrongPassword = req.session.wrongPassword
+    delete req.session.userNoExist
+    delete req.session.wrongPassword
         res.render('landing',);
  })
  
  //-----------LOGIN PAGE-----------//
  sessions.get('/login', (req, res)=> {
-     res.render('login.ejs')
+     res.render('login.ejs',
+        {
+            userError:[],
+            passError: []
+        }
+     )
  })
  
  //-----------LOGIN-POST--------------//
  sessions.post('/login', async (req, res)=> {
      const {username, password} = req.body; 
      console.log(req.body);
+
+     let userError = []
+     let passError = []
      //check user exists
      const user = await UserModel.findOne({username})
      
- 
      if(!user) {
-         return res.redirect('./login')
-     }
+         userError.push({message: 'This username does not exist. Did you type it in correctly?'})
+         res.render('login', 
+         {
+             userError: userError,
+            passError: []
+         })
+     } else{
  
      //compare passwords
      const isMatch = await bcrypt.compare(password, user.password)
  
      if(!isMatch){
-
-         return res.redirect('./login')
+        passError.push({message: 'This password does not match the username. Did you type it in correctly?'})
+        res.render('login', 
+        {
+            passError: passError,
+            userError:[]
+        })
      }
      
      req.session.isAuth = true
@@ -67,28 +92,41 @@ sessions.get('/', (req, res)=> {
 
             });
         })
+    
        
      })
+    }
      
  })
  
  
- 
+ //----------REGISTER-GET-ROUTE------------//
  sessions.get('/register', (req, res)=> {
-     res.render('register.ejs')
+     res.render('register.ejs',
+        {
+            error:[]
+        }
+     )
  })
  
+ //----------REGISTER-POST-ROUTE------------//
  sessions.post('/register', async (req, res)=> {
      const {username, password} = req.body
      console.log(req.body);
- 
+    
+     let errors = []
+
      //to make sure two accounts don't register with the same username
      let user = await UserModel.findOne({username})
  
      if (user) {
-         return res.redirect ('/register')
-     }
- 
+        //  return res.redirect ('/register')
+        errors.push({message: 'This username is not avilable, please choose another'})
+        res.render('register', 
+        {
+            error: errors
+        })
+     } else {
      const hashedPsw = await bcrypt.hash(password, 12)
  
      user = new UserModel({
@@ -100,14 +138,15 @@ sessions.get('/', (req, res)=> {
      //mongoose method that saves the user that has been created into the database
 
      res.redirect('./login')
+    }
  })
 
  
- sessions.get('../myMusic', isAuth, (req, res)=> {
-     res.render('../myMusic',)
- })
+//  sessions.get('../myMusic', isAuth, (req, res)=> {
+//      res.render('../myMusic',)
+//  })
  
- //package to logout
+ //----------LOGOUT-POST-ROUTE------------//
  sessions.post('/logout', (req, res)=> {
      req.session.destroy((err)=> {
          if(err) throw err;
